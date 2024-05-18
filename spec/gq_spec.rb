60be4ccb8a14b39513c500154f6d56f2c9c579a6
swordsmanluke <lukeis@rocketmail.com>
2024-05-18 16:32:13 -0700

    Reset project with Idea - nothing major (#6)
    * Set required ruby version
    
    * Reset project with Idea - nothing major

---
+++ spec/gq_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gq do
+  it "has a version number" do
+    expect(Gq::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
