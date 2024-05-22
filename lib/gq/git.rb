4397cd2463859696f8d6651c8f90363a1ae139fc
lucas <lucas@perfectlunacy.com>
2024-05-21 17:20:48 -0700

    uhhh, let's try this

--- lib/gq/git.rb
+++ lib/gq/git.rb
@@ -200,7 +200,7 @@ class Git
     puts "Renaming #{old_name.cyan} to #{new_name.cyan}..."
     current_branch do
       checkout(old_name)
-      bash("git branch -m #{new_name}")
+      return bash("git branch -m #{new_name}")
     end
   end
 
